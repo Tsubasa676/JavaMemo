40a7c914ddd8308590c6e3f0374781e4b469d172
50saitamajp <7961327+Tsubasa676@users.noreply.github.com>
2017-08-15 17:11:41 +0900

    再帰式

---
+++ Saiki.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Saiki {
+    public static void main(String[] args){
+        int no = 4;
+        int co = 1;
+        System.out.println("・「" + no +"」を使った再帰");
+        Saiki(no , co);
+    }
+
+    private static void Saiki(int no, int co) {
+        for(int i = 1; i <= no; i++){
+            if(co!=1) {
+                for (int j = 1; j < co; j++) {
+                    System.out.print("   ");
+                }
+            }
+            //System.out.println("i = " + i);
+            System.out.println(i);
+            if(no != co){
+                co ++;
+                Saiki(no,co);
+                co--;
+            }
+        }
+    }
+}
